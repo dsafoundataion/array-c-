52f742fc2d55d73e3e96d4b1bff3842dd0398009
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-19 14:29:11 +0530

    Create increaseIQ.Problem.cpp

---
+++ increaseIQ.Problem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int currentIq = x+7;
+	if(currentIq>170){
+	    cout<<"Yes"<<endl;
+	}else{
+	    cout<<"No"<<endl;
+	    
+	}
+	return 0;
+}
