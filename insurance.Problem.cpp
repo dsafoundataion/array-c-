40c59bf00ce68a04ee2350edcc8cf6da00f53e64
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-19 14:10:20 +0530

    Create insurance.Problem.cpp

---
+++ insurance.Problem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<y<<endl;
+	    }else{
+	        cout<<x<<endl;
+	   }
+	}
+	return 0;
+}
